33fd7fb692a4f4b6d9eca4ca0b4cc0adb86f1672
Mohammed Irshad <irshad.hasmat@gmail.com>
2013-12-26 23:21:06 +0530

    Last Time Changes

---
+++ mMedicine Plus/settings/list.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mMedicine_Plus.settings
+{
+    public partial class list : Form
+    {
+        public list()
+        {
+            InitializeComponent();
+        }
+
+        private void listItem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
